fa80b7e72713b30084b10d3ba761cdeede725d45
Vit <hutki@tut.by>
2017-03-28 09:13:11 +0300

    123

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+include "lock.php";
+?>
+<!DOCTYPE html>
+<head lang="ru">
+    <meta charset="UTF-8">
+    <title>Главная страница блока администратора</title>
+       
+      <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    
+</body>
+</html>

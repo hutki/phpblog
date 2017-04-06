3fc589cd039c7008bd89f4d3757310b707342610
Vit <hutki@tut.by>
2017-04-06 17:11:47 +0300

    14

--- view_posts.php
+++ view_posts.php
@@ -59,8 +59,43 @@ else {
                     <p>Количество просмотров: %s</p>
 
                     ",$myrow['title'],$myrow['date'],$myrow['author'],$myrow['text'],$myrow['view']);
+                    //вывод коментариев
+                  echo "<p class='comm_title'>Комментарии к этой заметке:</p>";
+                  $result3 = mysqli_query($db, "SELECT * FROM comments WHERE post='$id'");
+                  if (mysqli_num_rows($result3)>0) {
+                    $myrow3 = mysqli_fetch_array($result3);
+                  
+                  do {
+                      printf ("
+                        <div class='comm_wrap'>
+                        <p class='comm_auth'>Комментарий добавил: <b>%s</b></p>
+                        <p class='comm_date'>Дата: <b>%s</b></p>
+                        <p class='comm_text'>%s</p>
+                        </div>
+                       ",
+                       $myrow3['author'],$myrow3['date'],$myrow3['text']);
+                  }
+                  while($myrow3 = mysqli_fetch_array($result3));
+                  } 
 
+                  $result4 = mysqli_query ($db, "SELECT img FROM comments_setting");
+                  $myrow4 = mysqli_fetch_array($result4);
                 ?>
+
+                <p class='comm_title'> Добавить Ваш комментарий</p>
+                <form method="post" action="comment.php" name="form_com">
+                  <p><label>Ваше имя:</label>
+                   <input type="text" name="Author" size="30" maxlength="40"></p>
+                   <p><label>Текст комментария: <br><textarea name="text" cols="33" rows="4"></textarea></label></p>
+                    <p>
+                    
+                      <button type="submit" name="sub_com" value="Комментировать">Комментрировать</button>
+                    </p>
+                    <p>Введите сумму чисел с картинки</p>
+                    <p><img src="<?php echo $myrow4['img'] ?>"><input type="text" name="pr" size="5" maxlength="5" style="margin-top: 7px;vertical-align: top;"></p>
+                   
+
+                </form>
 
             </td>
         </tr>
